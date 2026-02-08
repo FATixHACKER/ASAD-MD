4d2642faa37853ff6480f10f4d8d1bbdef028fe0
FATixHACKER <asajut560089@gmail.com>
2026-02-08 20:33:06 +0500

    Implement antiCheck function for URL warnings

---
+++ utils/anti.js
@@ -0,0 +1,20 @@
+const warns = new Map();
+
+export async function antiCheck(sock, msg, text) {
+  const jid = msg.key.remoteJid;
+  const user = msg.key.participant;
+
+  if (/https?:\/\//i.test(text)) {
+    await sock.sendMessage(jid, { delete: msg.key });
+
+    const w = (warns.get(user) || 0) + 1;
+    warns.set(user, w);
+
+    if (w >= 2) {
+      await sock.groupParticipantsUpdate(jid, [user], "remove");
+      warns.delete(user);
+    }
+    return true;
+  }
+  return false;
+}
